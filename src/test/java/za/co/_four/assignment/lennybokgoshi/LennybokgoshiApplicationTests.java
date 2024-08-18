63f1418290f3f64aec0a9db8a9c5524f0f7a45b1
LennyBokgoshi <lenny@anylytical.co.za>
2024-08-18 14:38:32 +0200

    init commit

---
+++ src/test/java/za/co/_four/assignment/lennybokgoshi/LennybokgoshiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co._four.assignment.lennybokgoshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennybokgoshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
